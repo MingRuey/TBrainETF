1ce22b11e451534484c8a181596c66af72e58cc9
MRChou <imchou239@gmail.com>
2018-04-13 17:51:21 +0800

    First commit!

---
+++ README.md
@@ -0,0 +1,3 @@
+Code for TBrain ETF Prediction Competition
+Created on Fri Apr 13 16:49:41 2018
+@author: MRChou
---
+++ calconstituents.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Usage: Directly caculate ETF price from its constituents.
+
+"""
+
+def cacul():
+    return None
